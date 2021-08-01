fca72624eea822865e133975219bdad14a53869f
Douglas Marodin <douglasmarodin@gmail.com>
2021-08-01 12:38:59 -0300

    Adicionado docker-compose inicial e estrutura base do backend node

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = require('./routes');
+
+class App {
+    constructor() {
+        this.initServer();
+    }
+
+    initServer() {
+        this.express = express();
+        this.express.use(express.json());
+        this.express.use(router);
+    }
+}
+
+module.exports = App;
