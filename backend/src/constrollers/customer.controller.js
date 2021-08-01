3356b9f2bf8b2bb830a2772b75840b5381c7321e
Douglas Marodin <douglasmarodin@gmail.com>
2021-08-01 20:54:16 -0300

    Transferindo regras de negócio para a camada de serviço

--- backend/src/constrollers/customer.controller.js
+++ backend/src/constrollers/customer.controller.js
@@ -1,45 +1,29 @@
 const { fn, col, Op } = require("sequelize");
 const customerModel = require("../models/customer.model");
+const customer = require("../services/customer");
+const service = require("../services/customer");
 
 const countCustomersByCity = async (_, res) => {
-    const cities = await customerModel.findAll({
-        attributes: [
-            "city",
-            [fn('COUNT', col('id')), 'customers_total']
-        ],
-        group: "city"
-    });
-
+    const cities = await service.getNumClientsByCities();
     res.send(cities);
 }
 
 const customerById = async(req, res) => {
-    const customer = await customerModel.findByPk(req.params.id);
+    const customer = await service.getCustomerById(req.params.id);
     res.send(customer);
 }
 
 const customersByCity = async (req, res) => {
-    const customers = await customerModel.findAll({
-        where: {
-            city: {
-                [Op.like]: `%${req.params.city}%`
-            }
-        }
-    });
-
+    const customers = await service.getCustomersByCity(req.params.city);
     res.send(customers);
 }
 
 const updateCustomerById = async (req, res) => {
     const data = req.body;
 
-    const result = await customerModel.update(data, {
-        where: {
-            id: req.params.id
-        }
-    });
+    await service.updateCustomer(req.params.id, data);
 
-    res.send(result);
+    res.send({});
 }
 
 module.exports = {
---
+++ backend/src/services/customer.js
@@ -0,0 +1,40 @@
+const { fn, col, Op } = require("sequelize");
+const customerModel = require("../models/customer.model");
+
+module.exports = {
+  async getNumClientsByCities() {
+    const customers = await customerModel.findAll({
+      attributes: ["city", [fn("COUNT", col("id")), "customers_total"]],
+      group: "city",
+    });
+
+    return customers;
+  },
+
+  async getCustomerById(id) {
+    return await customerModel.findByPk(id);
+  },
+
+  async getCustomersByCity(city) {
+    const customers = await customerModel.findAll({
+        where: {
+            city: {
+                [Op.like]: `%${city}%`
+            }
+        }
+    });
+
+    return customers;
+  },
+
+  async updateCustomer(id, data) {
+      console.log(id)
+    const result = await customerModel.update(data, {
+        where: {
+            id: id
+        }
+    });
+
+    return result;
+  }
+};
