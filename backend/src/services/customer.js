9ceb9b5b31eeba053acff2ef63f6ba4a808113fc
Douglas Marodin <douglasmarodin@gmail.com>
2021-08-04 03:20:46 -0300

    Adequação ao requerimento de dados esperados nos retornos da API

--- backend/src/services/customer.js
+++ backend/src/services/customer.js
@@ -1,10 +1,11 @@
-const { fn, col, Op } = require("sequelize");
+const { fn, col, Op, literal } = require("sequelize");
 const customerModel = require("../models/customer.model");
 
 module.exports = {
   async getNumClientsByCities() {
     const customers = await customerModel.findAll({
       attributes: ["city", [fn("COUNT", col("id")), "customers_total"]],
+      order: [[literal("customers_total"), "DESC"], ["city", "ASC"]],
       group: "city",
     });
 
@@ -12,16 +13,20 @@ module.exports = {
   },
 
   async getCustomerById(id) {
-    return await customerModel.findByPk(id);
+    return await customerModel.findByPk(id, {
+      attributes: { exclude: ["createdAt", "updatedAt"] }
+    });
   },
 
   async getCustomersByCity(city) {
     const customers = await customerModel.findAll({
+        attributes: ["id", "first_name", "last_name", "email", "company"],
         where: {
             city: {
                 [Op.like]: `%${city}%`
             }
-        }
+        },
+        order: [["first_name", "ASC"]]
     });
 
     return customers;
